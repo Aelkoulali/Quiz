4bc2396840d3f3bda299c34109e265d403b2c0d7
Aelkoulali <abdellah.elkoulali@gmx.fr>
2023-08-16 10:29:18 +0200

    Add Question js

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which attribute must have a unique value each time it is used in an HTML document?",
+    answer: "Id",
+    options: [
+      "Title",
+      "Class",
+      "Style",
+      "Id"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which choice is not part of CSS box model?",
+    answer: "Paragraph",
+    options: [
+      "Margin",
+      "Paragraph",
+      "Border",
+      "Padding"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "Script" ,
+    options: [
+      "Javascript",
+      "Script",
+      "Js",
+      "Scripting"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which choice is not a category of report offered by Lighthouse tool?",
+    answer: "UX design",
+    options: [
+      "Performance",
+      "Accessibilty",
+      "UX design",
+      "SEO"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var carName;",
+    options: [
+      "variable carName;",
+      "v carName;",
+      "var carName",
+      "vari carName"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
